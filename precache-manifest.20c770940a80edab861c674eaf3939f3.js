5225ea9c15d481180d197f3c3aff1919fd443be5
bumsuk980119 <beom.kang17@gmail.com>
2020-06-25 21:52:50 +0900

    Updates

---
+++ precache-manifest.20c770940a80edab861c674eaf3939f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0b0baf2c711269f06a36c653818ba8",
+    "url": "/watch-lck-together/index.html"
+  },
+  {
+    "revision": "c3876b04f3dbcd913496",
+    "url": "/watch-lck-together/static/css/main.10b67c58.chunk.css"
+  },
+  {
+    "revision": "638c5f422dffcfad8944",
+    "url": "/watch-lck-together/static/js/2.4d3b7932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watch-lck-together/static/js/2.4d3b7932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3876b04f3dbcd913496",
+    "url": "/watch-lck-together/static/js/main.0a7e35f4.chunk.js"
+  },
+  {
+    "revision": "d2e335df6a38698f5a83",
+    "url": "/watch-lck-together/static/js/runtime-main.ca5caae7.js"
+  }
+]);
